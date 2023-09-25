7a4d98bd0b7637041781d8d80e6182fc42825a34
miltonbolonha <miltonbolonha@gmail.com>
2023-09-25 14:44:44 -0300

    bugfix:theme folder nextjs config

--- next-boilerplate/next.config.js
+++ next-boilerplate/next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const isGithubPages = process.env.GITHUB_PAGES || false;
-
+const repo = "naice-fotografia";
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: true,
-  basePath: isGithubPages ? "/next-boilerplate" : undefined,
+  basePath: isGithubPages ? "/" + repo : undefined,
   images: {
     loader: "custom",
     loaderFile: "./src/containers/imgLoaderContainer.js",
   },
   env: {
     IS_GITHUB_PAGE: isGithubPages,
-    THEME_FOLDER: "next-boilerplate",
+    THEME_FOLDER: repo,
   },
 };
 
